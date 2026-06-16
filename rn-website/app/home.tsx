06a6287060c9ea92614f58afbe7af0199846ebec
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-16 11:38:26 +0530

    Update home.tsx

--- rn-website/app/home.tsx
+++ rn-website/app/home.tsx
@@ -1,13 +1,14 @@
 "use client";
 import GlassDock from "@/components/ui/glass-dock";
 import FlipText from "@/components/ui/flip-text";
-import { Home as HomeIcon, Github, Instagram, Mail, MessageCircle, } from "lucide-react";
+import { Home as HomeIcon, Mail, MessageCircle } from "lucide-react";
+import { FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   const dockItems = [
     { title: "Home", icon: HomeIcon, href: "#" },
-    { title: "GitHub", icon: Github, href: "https://github.com/RoboNexxus" },
-    { title: "Instagram", icon: Instagram, href: "https://www.instagram.com/robonexus.ais46/" },
+    { title: "GitHub", icon: FaGithub, href: "https://github.com/RoboNexxus" },
+    { title: "Instagram", icon: FaInstagram, href: "https://www.instagram.com/robonexus.ais46/" },
     { title: "Mail", icon: Mail, href: "mailto:robonexus.ais46@gmail.com" },
     { title: "Social", icon: MessageCircle, href: "/contact" },
   ];
