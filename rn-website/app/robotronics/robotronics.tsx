37f852d34d6a9836fd22b1aa6f390721b1b45afd
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-07-03 16:30:12 +0530

    Create robotronics.tsx

---
+++ rn-website/app/robotronics/robotronics.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { redirect } from 'next/navigation'
+
+export default function Robotronics() {
+  return (
+    <main>
+        redirect("https://robotronics-ten.vercel.app/")
+    </main>
+  );
+}
